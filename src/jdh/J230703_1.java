2436d646ab157d4adae531fcd0a876d560f3547d
newdana01 <newdana01@gmail.com>
2023-07-03 15:46:54 +0900

    J230703_1 리팩터링 (924ms -> 448ms)

--- src/jdh/J230703_1.java
+++ src/jdh/J230703_1.java
@@ -3,30 +3,29 @@ package jdh;
 
 import java.util.*;
 import java.io.*;
+import java.lang.StringBuilder;
 
 public class J230703_1 {
     public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int n = Integer.parseInt(br.readLine());
-        String[][] arr = new String[n][2];
+        StringBuilder[] people = new StringBuilder[200];
 
-        for (int i = 0; i < n; i++){
-            st = new StringTokenizer(br.readLine());
-            arr[i][0] = st.nextToken();
-            arr[i][1] = st.nextToken();
+        for (int i = 0; i < people.length; i++) {
+            people[i] = new StringBuilder();
         }
 
-        Arrays.sort(arr, (a, b) -> {
-            int tempA = Integer.parseInt(a[0]);
-            int tempB = Integer.parseInt(b[0]);
-            if (tempA == tempB) return 0;
-            else return tempA - tempB;
-        });
-
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int age = Integer.parseInt(st.nextToken());
+            String name = st.nextToken();
+            people[age].append(age).append(" ").append(name).append("\n");
+        }
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++){
-            sb.append(arr[i][0] + " " + arr[i][1] + "\n");
+
+        for (StringBuilder p : people) {
+            sb.append(p);
         }
         System.out.println(sb);
     }
