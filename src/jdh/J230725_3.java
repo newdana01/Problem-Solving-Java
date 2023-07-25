f336a696bb285eda31c12fbccf090d1ea248e6cd
newdana01 <newdana01@gmail.com>
2023-07-25 14:50:04 +0900

    J230725_3 추가

---
+++ src/jdh/J230725_3.java
@@ -0,0 +1,57 @@
+// https://www.hackerrank.com/challenges/birthday-cake-candles/problem
+package jdh;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'birthdayCakeCandles' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY candles as parameter.
+     */
+
+    public static int birthdayCakeCandles(List<Integer> candles) {
+        // Write your code here
+        Collections.sort(candles);
+        int n = candles.size() - 1;
+        int tallest = candles.get(n);
+        int count = 1;
+
+        for (int i = n-1; i >= 0; i--){
+            if (candles.get(i) == tallest){
+                count++;
+            }else{
+                break;
+            }
+        }
+
+        return count;
+    }
+
+}
+
+public class J230725_3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int candlesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> candles = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.birthdayCakeCandles(candles);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
